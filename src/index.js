adf91d5a20dfd3c6fe790d6571778b7e070d53f2
rupav jain <rupavwinchester@gmail.com>
2018-03-25 10:18:08 +0530

    Functional app with undo options!

--- src/index.js
+++ src/index.js
@@ -71,11 +71,12 @@ class Game extends React.Component {
         squares: Array(9).fill(null),
       }],
       xIsNext: true,
+      stepNumber: 0,
     };
   }
 
   handleClick(i){
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -88,14 +89,23 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
 
+
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xiSNext: (step % 2) === 0,
+    })
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     let status;
     if (winner) {
@@ -109,7 +119,7 @@ class Game extends React.Component {
         'Go to move #' + move:
         'Go to game start';
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>
             {desc}
           </button>
