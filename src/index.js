d04a4c674d2cc00eea1e8aed0565900fd8e6460d
rupav jain <rupavwinchester@gmail.com>
2020-03-11 21:19:03 +0530

    remove hardcoded table

--- src/index.js
+++ src/index.js
@@ -40,24 +40,22 @@ class Board extends React.Component {
    );
   }
 
+  createTable  = () => {
+    let table = []
+    for(let i=0; i<3; i++){
+      let row = []
+      for(let j=0; j<3; j++){
+        row.push(<td>{this.renderSquare(3*i + j)}</td>)
+      }
+      table.push(<tr className="board-row">{row}</tr>)
+    }
+    return table
+  }
+
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.createTable()}
       </div>
     );
   }
